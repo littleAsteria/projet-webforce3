2bb357dd4eba6380f129f434c40911b58b61aa04
littleAsteria <cinnamonpan@gmail.com>
2017-07-12 14:28:58 +0200

    mis en place du projet
    Création du projet git
    Création du dossier game_project sur net beans
    Installation doctrine
    Installation vendor
    création de la base de données

---
+++ game.sql
@@ -0,0 +1,89 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Mer 12 Juillet 2017 à 14:20
+-- Version du serveur :  10.1.13-MariaDB
+-- Version de PHP :  5.6.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `game`
+--
+CREATE DATABASE IF NOT EXISTS game;
+
+USE game;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `membre`
+
+CREATE TABLE `membre` (
+  `id_membre` int(11) NOT NULL,
+  `pseudo` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `mdp` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `score` int(15) NOT NULL DEFAULT '0',
+  `statut_membre` enum('0','1') COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `question`
+--
+
+CREATE TABLE `question` (
+  `id_question` int(11) NOT NULL,
+  `question` text COLLATE utf8_unicode_ci NOT NULL,
+  `reponse_a` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `reponse_b` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `reponse_c` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `reponse_d` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `bonne_reponse` enum('a','b','c','d') COLLATE utf8_unicode_ci NOT NULL,
+  `niveau` int(2) NOT NULL,
+  `statut_question` enum('0','1') COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `membre`
+--
+ALTER TABLE `membre`
+  ADD PRIMARY KEY (`id_membre`);
+
+--
+-- Index pour la table `question`
+--
+ALTER TABLE `question`
+  ADD PRIMARY KEY (`id_question`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `membre`
+--
+ALTER TABLE `membre`
+  MODIFY `id_membre` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `question`
+--
+ALTER TABLE `question`
+  MODIFY `id_question` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
