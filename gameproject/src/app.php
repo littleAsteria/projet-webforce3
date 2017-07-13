92f7b5ff973d188fd2747fa152274b3146e74340
David <spanjaard.david@gmail.com>
2017-07-13 14:46:18 +0200

    Mise en place de certaines vues
    -Mise en place de la vue game
    -Mise en place de la vue regles
    -Mise en place du GameController

--- gameproject/src/app.php
+++ gameproject/src/app.php
@@ -1,11 +1,13 @@
 <?php
 
+use Controller\GameController;
 use Silex\Application;
 use Silex\Provider\AssetServiceProvider;
-use Silex\Provider\TwigServiceProvider;
-use Silex\Provider\ServiceControllerServiceProvider;
-use Silex\Provider\HttpFragmentServiceProvider;
 use Silex\Provider\DoctrineServiceProvider;
+use Silex\Provider\HttpFragmentServiceProvider;
+use Silex\Provider\ServiceControllerServiceProvider;
+use Silex\Provider\SessionServiceProvider;
+use Silex\Provider\TwigServiceProvider;
 
 $app = new Application();
 $app->register(new ServiceControllerServiceProvider());
@@ -33,5 +35,9 @@ $app->register( //nom de méthode propre à silex
 );
 
 $app->register(new SessionServiceProvider());
+
+$app['game.controller'] = function () use ($app){
+    return new GameController($app);
+};
 
 return $app;
