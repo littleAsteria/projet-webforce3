937847f9996f450402bb2d0382199cf6af7f0504
Sinostra <spanjaard.david@gmail.com>
2017-07-18 12:42:34 +0200

    Ajout de quelques commentaires

--- gameproject/src/Controller/ControllerQuestionAjax.php
+++ gameproject/src/Controller/ControllerQuestionAjax.php
@@ -9,12 +9,16 @@ class ControllerQuestionAjax extends ControllerAbstract{
    
     public function getRandomQuestion(){
         
+        //La difficulté voulue sera envoyée par le data de la requête ajax
         $difficulty = $_GET['difficulty'];
         
+        //Va chercher toutes les question de la difficulté passée en paramètre qui ont un statut de 1
         $questions = $this->app['question.repository']->getQuestionsByDifficulty($difficulty);
         
+        //Récupère une question au hasard dans le tableau $questions
         $randomQuestion = $questions[array_rand($questions)];
 
+        //Retourne des données de cette question en json
         return $this->app->json(
                 [
                     'question' => $randomQuestion->getQuestion(),
