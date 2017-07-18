f36ce95dcc775bc1e4209f1bba3f028b94655421
Sinostra <spanjaard.david@gmail.com>
2017-07-18 16:07:55 +0200

    Vérification de la réponse donnée par le joueur

--- gameproject/web/assets/js/game.js
+++ gameproject/web/assets/js/game.js
@@ -4,10 +4,34 @@ $(function(){
     var startingDifficulty = 1;
     var currentDifficulty = startingDifficulty;
     
+    var chosenAnswer;
+    
     getRandomQuestion(currentDifficulty, function(data){
         currentQuestion = data;
-        console.log(currentQuestion);
+        
     });
+    
+    $('.reponseButton').on('click', function(e){
+        chosenAnswer = $(this).attr('id').substr(-1).toLowerCase();
+    });
+    
+    $('#valider').on('click', function(e){
+        if(chosenAnswer != undefined){
+            if(verificationReponse(chosenAnswer, currentQuestion)){
+                console.log('bonne réponse !')
+            }
+                
+            else {
+                console.log('mauvaise réponse !')
+            }
+        }
+
+        else {
+            console.log('aucune réponse donnée');
+        }
+    });
+    
+    
 
     
 });
