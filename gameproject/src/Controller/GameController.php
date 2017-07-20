74d52aa0dd8ba37d4984ff6d969e379472826f29
littleAsteria <cinnamonpan@gmail.com>
2017-07-20 10:18:20 +0200

    commentaires part2

--- gameproject/src/Controller/GameController.php
+++ gameproject/src/Controller/GameController.php
@@ -30,22 +30,27 @@ class GameController extends ControllerAbstract{
     faire une requete pour envoyer le score actuel dans la table membre 
     Sinon ne pas envoyé le score
      */
+    
     public function postBestScore(){
         
-        //enregistrement du score dans la session
+        //enregistrement du score dans la session qu'on soit connecté ou non
+        //on s'en sert  
         $this->app['membre.manager']->saveScore($_POST['scoreEnvoye']);
         
+        //Si le jouer est connecté et est donc membre:
         if($this->app['session']->has('membre')){
             
 
             //on récupére le score du membre connecté et on le compare au score envoyé par l'ajax
             if($this->app['membre.manager']->getMembre()->getScore() < $_POST['scoreEnvoye']){
 
-                //On enregistre le score en BDD
+                //Si le score actuel est supérieur au score enregistré en BDD, on le remplace 
                 $this->app['membre.repository']->setScore($this->app['membre.manager']->getMembre()->getId_membre(), $_POST['scoreEnvoye']);
+                
+                //nouveau score enregistré en session
+                $this->app['membre.manager']->getMembre()->setScore($_POST['scoreEnvoye']);
             }
-
-            $this->app['membre.manager']->getMembre()->setScore($_POST['scoreEnvoye']);
+            
 
        
         }
--- gameproject/web/assets/js/PostBestScore.js
+++ gameproject/web/assets/js/PostBestScore.js
@@ -1,17 +1,17 @@
+//On récupérer le score du joueur à la fin du jeu pour l'envoyer dans la BDD via la méthode ajax 
 function postScore(score){
         
     $.ajax({
            
         method : 'post',
-        url: path2,
-        data:{scoreEnvoye:score}
+        url: path2,//action de la route best-score 
         
                
     })
     .done(function(data){
         //console.log('done');
         window.location = path3;
-        
+        //route qui renvoie à la page de fin de partie
     })
     
     .fail(function(jqXHR, textStatus){
