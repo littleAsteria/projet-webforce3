54d5097b0c3ace4a5380a7ab33d5325220a511b0
David <spanjaard.david@gmail.com>
2017-07-22 19:54:04 +0200

    Refonte du css de la page game et ajout de sons

---
+++ gameproject/web/assets/js/sound.js
@@ -0,0 +1,18 @@
+var errorSound = new Howl({
+    src:['../assets/son_musique/son/answer_error.ogg']
+});
+
+var correctSound = new Howl({
+    src:['../assets/son_musique/son/answer_correct.wav']
+});
+
+var comboSound = new Howl({
+    src:['../assets/son_musique/son/answer_combo.ogg']
+});
+
+var gameButtonClickSound = new Howl({
+    src:['../assets/son_musique/son/click_button_game.ogg']
+});
+
+
+
